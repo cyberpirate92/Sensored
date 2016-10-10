e18ff1ee2c528e5a0ceb86805c40c50c27943001
raviteja <bsravi.tez@gmail.com>
2016-10-10 09:26:06 +0530

    Added few more parameters for Gyroscope

--- app/src/main/java/com/example/raviteja/sensoredp/GyroActivity.java
+++ app/src/main/java/com/example/raviteja/sensoredp/GyroActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
@@ -17,7 +20,10 @@ public class GyroActivity extends Activity implements SensorEventListener {
     private SensorManager manager;
     private Sensor mGyroscope;
     private TextView xAxisTextView, yAxisTextView, zAxisTextView;
+    private TextView xAxisMaxView, yAxisMaxView, zAxisMaxView;
     private float xAxis, yAxis, zAxis;
+    private float xMax, yMax, zMax;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +37,42 @@ public class GyroActivity extends Activity implements SensorEventListener {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+        xMax = yMax = zMax = 0;
         xAxisTextView = (TextView) findViewById(R.id.textView9);
         yAxisTextView = (TextView) findViewById(R.id.textView10);
         zAxisTextView = (TextView) findViewById(R.id.textView11);
+        xAxisMaxView = (TextView) findViewById(R.id.textView14);
+        yAxisMaxView = (TextView) findViewById(R.id.textView15);
+        zAxisMaxView = (TextView) findViewById(R.id.textView16);
     }
 
 
     public void setxAxis(float xAxis) {
         this.xAxis = xAxis;
         xAxisTextView.setText(String.format(Locale.ENGLISH, "%.2f", this.xAxis));
+        if(xMax < Math.abs(xAxis)) {
+            xMax = Math.abs(xAxis);
+            xAxisMaxView.setText(String.format(Locale.ENGLISH, "%.2f", this.xMax));
+        }
+
     }
 
     public void setyAxis(float yAxis) {
         this.yAxis = yAxis;
         yAxisTextView.setText(String.format(Locale.ENGLISH, "%.2f", this.yAxis));
+        if(yMax < Math.abs(yAxis)) {
+            yMax = Math.abs(yAxis);
+            yAxisMaxView.setText(String.format(Locale.ENGLISH, "%.2f", this.yMax));
+        }
     }
 
     public void setzAxis(float zAxis) {
         this.zAxis = zAxis;
         zAxisTextView.setText(String.format(Locale.ENGLISH, "%.2f", this.zAxis));
+        if(zMax < Math.abs(zAxis)) {
+            zMax = Math.abs(zAxis);
+            zAxisMaxView.setText(String.format(Locale.ENGLISH, "%.2f", this.zMax));
+        }
     }
 
     @Override
@@ -75,5 +98,9 @@ public class GyroActivity extends Activity implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    public void resetMaxValues(View view) {
+        this.xMax = this.yMax = this.zMax = 0;
     }
 }
